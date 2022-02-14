562c82f4321287fcb2a09230cb094ae8f11f319a
Vanessa Reese <88684757+Vinesse-Nevertheless@users.noreply.github.com>
2022-02-13 22:16:58 -0500

    module 2 But something is not quite right with QuizQuestion module quiz_question prop

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,18 +1,26 @@
-import React, {Component} from "react";
+//Parent component to QuizQuestion.js
 
+import React, {Component} from "react";
+import QuizQuestion from "./QuizQuestion.js"
+
+//Get quiz question number, question text, answer options and correct answer from json file
 let quizData = require('./quiz_data.json')
 
+
+//make data available in state for render
 class Quiz extends Component {
     constructor(props){
         super(props)
-        this.state = {quiz_position:1}
+        this.state = {
+            quiz_position:1
+            //quiz_question:quizData.quiz_questions[this.props.quiz_position-1]
+        }  
     }
+
+    //render quiz question
     render(){
-        return <div>
-           <div className = "QuizQuestion">
-             {quizData.quiz_questions[0].instruction_text}
-              
-           </div>
+        return <div className="QuizQuestion" >
+         <QuizQuestion quiz_question = {quizData.quiz_questions[0]} /> {/*[quiz.position-1] is what I thought but it doesn't work.  Maybe will find fix later.*/}
         </div>
     }
 }
