c88b708bf997cd393fc22627110a22cf088d6606
Vanessa Reese <88684757+Vinesse-Nevertheless@users.noreply.github.com>
2022-02-16 12:16:54 -0500

    project complete

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,22 +1,72 @@
+//child component to Quiz.js and QuizQuestionButton
 import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton.js";
 
+/*
+Button Flow 2
+
+The clickHandler prop in <QuizQuestionButton/> component below is bound the handleClick(buttonText)
+The binding allows handleClick in QuizQuestionButton.js to pass an argument to clickHandler prop on this page
+and to have handleClick(buttonText) implement logic.
+
+The logic: if button clicked equals the right answer, go to the method that handles showing the next question.
+
+Go to Quiz.js for part 3.
+*/
+
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
   render() {
+    {
+      /* RETURN REQUIRED */
+    }
     return (
       <main>
+        {" "}
+        {this.state.incorrectAnswer ? (
+          <p className="error"> Sorry that's not right </p>
+        ) : null}
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton
-              button_text={this.props.quiz_question.answer_options[0]}
-            />
+            {/* When you call this on an array, you can have it run through all the items in that array 
+          and do something interesting with them – in our case, returning a new array of JSX that can 
+          be drawn. 
+          
+          For every item in the array, it gives us the item itself in person and 
+          the position of the item in index.*/}
+            {this.props.quiz_question.answer_options.map(
+              (
+                answer_option,
+                index //creates a new anonymous function (that's the => part) that receives answer_option & index as a parameter and will return a value of the modified data.
+              ) => (
+                <QuizQuestionButton
+                  key={index}
+                  button_text={answer_option}
+                  clickHandler={this.handleClick.bind(this)}
+                />
+              )
+            )}{" "}
+            {/* React NEEDS a key attribute so that it knows which item is which. */}
           </ul>
         </section>
       </main>
     );
+  }
+  handleClick(buttonText) {
+    if (buttonText == this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false });
+      this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrectAnswer: true });
+    }
   }
 }
 
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
+//I think there is something wrong with the test for this final task.
+
+/*
+Button Flow 1
+
+onClick prop in the <button> tag binds the button to the handleClick() below the render().
+handleClick() then looks to QuizQuestion.js to lay out the logic for how to handle the click.
+handleClick() passes the fact that a button has been clicked to the clickHandler prop in QuizQuestion.js
+Go to QuizQuestion.js for part 2.
+*/
 
 class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
-        <button>{this.props.button_text}</button>
+        <button onClick={this.handleClick.bind(this)}>
+          {" "}
+          {this.props.button_text}{" "}
+        </button>{" "}
+        {/* Adds event handler that is bound to the method below. */}
       </li>
     );
+  }
+  //Make the buttons do something when clicked
+  handleClick() {
+    this.props.clickHandler(this.props.button_text);
   }
 }
 
